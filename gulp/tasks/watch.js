3443158fa52c88974db6cf67971999e4951afb83
Lee <lagjr22@gmail.com>
2018-11-24 01:10:54 -0500

    Installed and setup postcss-mixins. Used mixens to setup @media queries for responsive design setup.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,12 +5,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-browserSync = require('browser-sync').create();
+browserSync = require('browser-sync').create(),
+mixins = require('postcss-mixins');
 
 
 function styles() {
     return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
         console.log(errorInfo.toString());
         this.emit('end');
